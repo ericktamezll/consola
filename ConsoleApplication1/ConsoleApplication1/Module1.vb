a6017274146eea770ca6645e2d89e050adcb29d5
Erick Emmanuel Tamez Leal <erick.tamezll@gmail.com>
2016-05-04 23:31:29 -0500

    avance

---
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        WriteLine("hola k ase")
+        For i As Integer = 0 To 10 Step 1
+            WriteLine(i)
+
+        Next
+
+        WriteLine("putos")
+        ReadLine()
+    End Sub
+
+End Module
